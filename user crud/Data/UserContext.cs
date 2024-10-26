bad03795e73f3a7ec63afb5feec74864563c8d4d
viimsi <8240486@estg.ipp.pt>
2024-10-26 18:47:04 +0100

    Add project files.

---
+++ user crud/Data/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using user_crud.Models;
+
+namespace user_crud.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ user crud/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace user_crud.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string? Username { get; set; } = null;
+
+        [Required]
+        public string? Email { get; set; } = null;
+
+        public string? PhoneNumber { get; set; } = null;
+
+        public string? Address { get; set; } = null;
+
+        public string? Zip { get; set; } = null;
+
+        public string? City { get; set; } = null;
+    }
+}
